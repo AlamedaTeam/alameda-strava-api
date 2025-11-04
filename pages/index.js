94ef14a6c29508abe723f4880191d32512bc0a21
AlamedaTeam <amartincasero@gmail.com>
2025-11-04 23:29:57 +0100

    Add Home component to index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: "system-ui, sans-serif",
+      textAlign: "center",
+      marginTop: "20vh"
+    }}>
+      <h1>✅ Alameda Strava API</h1>
+      <p>Servidor conectado y funcionando correctamente.</p>
+    </div>
+  );
+}
