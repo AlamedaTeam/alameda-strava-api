99a0d981fbf4f65999a07ab6cb416f48d8d30182
AlamedaTeam <amartincasero@gmail.com>
2025-11-05 23:45:21 +0100

    Add Strava OAuth redirect handler

---
+++ pages/api/auth.ts
@@ -0,0 +1,11 @@
+// pages/api/auth.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET } = process.env;
+  const redirectUri = "https://alameda-strava-api.vercel.app/api/auth/callback";
+
+  const url = `https://www.strava.com/oauth/authorize?client_id=${STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${redirectUri}&approval_prompt=force&scope=read,activity:read_all,profile:read_all`;
+
+  res.redirect(url);
+}
