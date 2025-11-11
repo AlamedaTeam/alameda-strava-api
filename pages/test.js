8e816d861f82913f6e34c0718ed68d65a5900d4c
AlamedaTeam <amartincasero@gmail.com>
2025-11-12 00:14:24 +0100

    Create test.js

---
+++ pages/test.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ background: "black", color: "white", height: "100vh", padding: "40px" }}>
+      <h1>✅ RUTA INTERNA FUNCIONANDO</h1>
+      <p>Si ves esto, Next.js está leyendo correctamente las subrutas.</p>
+    </div>
+  );
+}
