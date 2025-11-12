ed55ef50ad015359d631732b05daf2193d433ac9
AlamedaTeam <amartincasero@gmail.com>
2025-11-12 22:50:58 +0100

    Create index.js

---
+++ pages/dashboard-vip/index.js
@@ -0,0 +1,17 @@
+export default function DashboardVip() {
+  return (
+    <div style={{
+      background: "black",
+      color: "lime",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "monospace",
+      fontSize: "22px"
+    }}>
+      ✅ Nueva ruta /dashboard-vip funcionando correctamente
+    </div>
+  );
+}
+
