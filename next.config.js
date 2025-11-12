080a6c86239d2fe849ba3691c14df7b64763b17b
AlamedaTeam <amartincasero@gmail.com>
2025-11-13 00:00:22 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  experimental: {
-    appDir: false, // 👈 fuerza modo pages
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/vip',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
